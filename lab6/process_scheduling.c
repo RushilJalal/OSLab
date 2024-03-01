f4d11a3742147b417816ea8159e288325e046b0b
Rushil Jalal <rushiljalal@gmail.com>
2024-03-01 08:13:02 +0530

    lab6

--- lab6/process_scheduling.c
+++ lab6/process_scheduling.c
@@ -7,6 +7,7 @@ typedef struct Process
     int arrival_time;
     int burst_time;
     int remaining_time;
+    int priority;
 } Process;
 
 int compareProcesses(Process *p1, Process *p2)
@@ -59,14 +60,91 @@ void RoundRobin(Process processes[], int n, int quantum)
     int currentTime = 0;
     int completedProcesses = 0;
 
-    while(completedProcesses < n)
+    while (completedProcesses < n)
     {
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            if(processes[i].remaining_time > 0)
+            if (processes[i].remaining_time > 0)
             {
-                
+                if (processes[i].remaining_time > quantum)
+                {
+                    currentTime += quantum;
+                    processes[i].remaining_time -= quantum;
+                    printf("Time: %d  Process: %d\n", currentTime, processes[i].process_id);
+                }
+                else
+                {
+                    currentTime += processes[i].remaining_time;
+                    processes[i].remaining_time = 0;
+                    printf("Time %d: Process %d\n", currentTime, processes[i].process_id);
+                    completedProcesses++;
+                }
             }
         }
     }
+}
+
+void priorityScheduling(Process processes[], int n)
+{
+    // Sort processes based on priority (non-preemptive)
+    qsort(processes, n, sizeof(Process), compareProcesses);
+
+    int currentTime = 0;
+
+    // Execute processes in order of priority
+    for (int i = 0; i < n; ++i)
+    {
+        currentTime += processes[i].burst_time;
+        printf("Time %d: Process %d\n", currentTime, processes[i].process_id);
+    }
+}
+
+int main()
+{
+    int n, quantum;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    Process processes[n];
+
+    for (int i = 0; i < n; ++i)
+    {
+        processes[i].process_id = i + 1;
+        printf("Enter arrival time, burst time, priority for Process %d: ", i + 1);
+        scanf("%d %d %d", &processes[i].arrival_time, &processes[i].burst_time, &processes[i].priority);
+        processes[i].remaining_time = processes[i].burst_time;
+    }
+
+    printf("\nSelect a scheduling algorithm:\n");
+    printf("1. Preemptive SJF (with FCFS for equal arrival time)\n");
+    printf("2. Round Robin\n");
+    printf("3. Non-Preemptive Priority\n");
+    printf("Enter your choice: ");
+
+    int choice;
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        printf("\nPreemptive SJF Schedule with FCFS tie-breaker:\n");
+        preemptiveSJF(processes, n);
+        break;
+    case 2:
+        printf("Enter the time quantum for Round Robin: ");
+        scanf("%d", &quantum);
+        printf("\nRound Robin Schedule:\n");
+        roundRobin(processes, n, quantum);
+        break;
+    case 3:
+        printf("\nNon-Preemptive Priority Schedule:\n");
+        priorityScheduling(processes, n);
+        break;
+    default:
+        printf("Invalid choice\n");
+        break;
+    }
+
+    return 0;
 }
