c5979183b8c2069bae25ed5fa730b929ac95b15f
Rushil Jalal <rushiljalal@gmail.com>
2024-03-11 23:49:04 +0530

    changes

--- lab5/q2.c
+++ lab5/q2.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <unistd.h>
 
 // swap 2 strings
 void swapStrings(char **a, char **b)
@@ -26,7 +27,7 @@ void bubbleSortStrings(int argc, char *argv[])
     }
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
     pid_t pid = fork();
     if (pid == -1)
@@ -37,6 +38,7 @@ int main(int argc, char const *argv[])
 
     else if (pid == 0) // child
     {
+        wait(NULL); // child should wait for parent to execute
         bubbleSortStrings(argc, argv);
         printf("Sorted strings in child process: \n");
         for (int i = 1; i < argc; i++)
@@ -47,7 +49,6 @@ int main(int argc, char const *argv[])
 
     else
     {
-        wait(NULL);
         printf("Unsorted strings in parent process: \n");
         for (int i = 1; i < argc; i++)
         {
@@ -56,4 +57,3 @@ int main(int argc, char const *argv[])
     }
     return 0;
 }
-
