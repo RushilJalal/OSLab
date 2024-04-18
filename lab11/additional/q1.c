f92e666c3a2d916ccb5eb150553f1a905886ded2
Rushil Jalal <rushiljalal@gmail.com>
2024-04-18 19:00:29 +0530

    final disk scheduling

--- lab11/additional/q1.c
+++ lab11/additional/q1.c
@@ -1,14 +1,33 @@
-/* Develop a menu driven program to simulate the following disk scheduling algo- 
+/* Develop a menu driven program to simulate the following disk scheduling algo-
 rithms: FCFS, LOOK. */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h> // to use INT_MAX
+
+void sort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
 // Function to simulate FCFS disk scheduling algorithm
-void fcfs(int *requests, int num_requests, int head_position) {
+void fcfs(int *requests, int num_requests, int head_position)
+{
     int total_seek_time = 0;
 
-    for (int i = 0; i < num_requests; i++) {
+    for (int i = 0; i < num_requests; i++)
+    {
         int seek_time = abs(requests[i] - head_position);
         total_seek_time += seek_time;
         head_position = requests[i];
@@ -19,73 +38,77 @@ void fcfs(int *requests, int num_requests, int head_position) {
 }
 
 // Function to simulate LOOK disk scheduling algorithm
-void look(int *requests, int num_requests, int head_position) {
-    int total_seek_time = 0;
-    int direction = 1; // 1 for moving towards higher track, -1 for moving towards lower track
+void look(int *requests, int num_requests, int head_position)
+{
+    int total_movement = 0;
+    int i;
 
-    // Sort the requests in ascending order
-    for (int i = 0; i < num_requests - 1; i++) {
-        for (int j = 0; j < num_requests - i - 1; j++) {
-            if (requests[j] > requests[j + 1]) {
-                int temp = requests[j];
-                requests[j] = requests[j + 1];
-                requests[j + 1] = temp;
-            }
-        }
-    }
-
-    // Find the index of head position in the sorted requests
-    int head_index = 0;
-    for (int i = 0; i < num_requests; i++) {
-        if (requests[i] >= head_position) {
-            head_index = i;
-            break;
-        }
-    }
-
-    // Move towards higher track until the last request
-    for (int i = head_index; i < num_requests; i++) {
-        int seek_time = abs(requests[i] - head_position);
-        total_seek_time += seek_time;
-        head_position = requests[i];
-    }
-
-    // Move towards lower track from the last request to the first request
-    for (int i = head_index - 1; i >= 0; i--) {
-        int seek_time = abs(requests[i] - head_position);
-        total_seek_time += seek_time;
-        head_position = requests[i];
-    }
-
+    sort(requests, num_requests);
     printf("LOOK Disk Scheduling Algorithm\n");
-    printf("Total Seek Time: %d\n", total_seek_time);
+    printf("LOOK Sequence: ");
+
+    // move right and check disk requests > initial head position
+    for (i = 0; i < num_requests; i++)
+    {
+        if (requests[i] >= head_position)
+        {
+            printf("%d ", requests[i]);
+            total_movement += abs(requests[i] - head_position);
+            head_position = requests[i];
+            requests[i] = INT_MAX;
+        }
+    }
+
+    // move left
+    while (i >= 0)
+    {
+        if (requests[i] < head_position && requests[i] != INT_MAX)
+        {
+            printf("%d ", requests[i]);
+            total_movement += abs(requests[i] - head_position);
+            head_position = requests[i];
+            requests[i] = INT_MAX;
+        }
+        i--;
+    }
+
+    printf("\nTotal Seek Time: %d\n", total_movement);
 }
 
-int main() {
+int main()
+{
     int num_requests;
     int head_position;
-
-    printf("Enter the number of disk requests: ");
-    scanf("%d", &num_requests);
-
-    int *requests = (int *)malloc(num_requests * sizeof(int));
-
-    printf("Enter the disk requests: ");
-    for (int i = 0; i < num_requests; i++) {
-        scanf("%d", &requests[i]);
-    }
 
     printf("Enter the initial head position: ");
     scanf("%d", &head_position);
 
-    int choice;
-    printf("Disk Scheduling Algorithms:\n");
-    printf("1. FCFS\n");
-    printf("2. LOOK\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
+    printf("Enter the number of disk requests: ");
+    scanf("%d", &num_requests);
 
-    switch (choice) {
+    int r[num_requests];
+
+    int *requests = malloc(num_requests * sizeof(int));
+
+    printf("Enter the disk requests: ");
+    for (int i = 0; i < num_requests; i++)
+    {
+        scanf("%d", &requests[i]);
+        r[i] = requests[i];
+    }
+
+    int choice;
+    do
+    {
+        printf("Disk Scheduling Algorithms:\n");
+        printf("1. FCFS\n");
+        printf("2. LOOK\n");
+        printf("-1. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
         case 1:
             fcfs(requests, num_requests, head_position);
             break;
@@ -95,9 +118,14 @@ int main() {
         default:
             printf("Invalid choice!\n");
             break;
-    }
+        }
 
-    free(requests);
+        // reinitialse r with requests array
+        for (int i = 0; i < num_requests; i++)
+        {
+            r[i] = requests[i];
+        }
+    } while (choice != -1);
 
     return 0;
 }
